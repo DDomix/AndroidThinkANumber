3e7538e85f900f56b537b66d7fe71378a8dd780e
DDomix <dominik.papp1010@gmail.com>
2022-10-07 10:03:49 +0200

    done

--- app/src/main/java/com/example/thinkanumber/MainActivity.java
+++ app/src/main/java/com/example/thinkanumber/MainActivity.java
@@ -1,174 +1,212 @@
 package com.example.thinkanumber;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
 
-    private Button plus;
-    private Button minus;
-    private Button tippgomb;
-    private ImageView hp1;
-    private ImageView hp2;
-    private ImageView hp3;
-    private ImageView hp4;
+    private ImageView imageHp1, imageHp2, imageHp3, imageHp4, imageHp5;
     private ImageView[] eletek;
-    private TextView tippertek;
-    private int tipp;
-    private int gondolt;
-    private int elet;
-    private Random rnd;
-    private AlertDialog.Builder builder;
-    private Button nehezgomb;
-    private Button konnyugomb;
-    private AlertDialog.Builder jatekVegeBuilder;
-    private AlertDialog.Builder nehezsegDialogBuilder;
+    private TextView textTippeltSzam;
+    private Button btnNovel, btnCsokkent, btnTippel, btnKonnyu, btnNehez;
+    private int gondoltSzam, tippeltSzam, elet;
+    private AlertDialog.Builder builderVege, builderNehezseg;
+    private Toast customToast;
+    private int maxSzam;
+    private boolean nehezseg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
-        addlisteners();
+
+        ujJatek();
+
+        btnTippel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (gondoltSzam < tippeltSzam){
+                    Toast.makeText(MainActivity.this,
+                            "A gondolt szám kisebb", Toast.LENGTH_SHORT).show();
+                    eletLevon();
+                } else if (gondoltSzam > tippeltSzam){
+                    Toast.makeText(MainActivity.this,
+                            "A gondolt szám nagyobb", Toast.LENGTH_SHORT).show();
+                    eletLevon();
+                } else {
+                    builderVege.setTitle("Győzelem");
+                    AlertDialog dialog = builderVege.create();
+                    dialog.show();
+                }
+            }
+        });
+        btnNovel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tippeltSzam < maxSzam){
+                    tippeltSzam++;
+                    textTippeltSzam.setText(""+tippeltSzam);
+                }else{
+                    Toast.makeText(MainActivity.this,
+                            "A szám nem lehet nagyobb mint 10", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnCsokkent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tippeltSzam > 1){
+                    tippeltSzam--;
+                    textTippeltSzam.setText(String.valueOf(tippeltSzam));
+                }else{
+                    Toast.makeText(MainActivity.this,
+                            "A szám nem lehet kisebb mint 1", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        btnKonnyu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nehezseg = false;
+                builderNehezseg.create().show();
+            }
+        });
+
+        btnNehez.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nehezseg = true;
+                builderNehezseg.create().show();
+            }
+        });
     }
 
-    private void addlisteners() {
-        plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (tipp<10){
-                    tipp++;
-                    tippertek.setText(String.valueOf(tipp));
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Nem lehet 10-nél nagyobb", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        minus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (tipp>1){
-                    tipp--;
-                    tippertek.setText(String.valueOf(tipp));
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Nem lehet kisebb mint 1", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        tippgomb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (tipp<gondolt){
-                    eletcsokkent();
-                    Toast.makeText(MainActivity.this, "A gondolt szám nagyobb", Toast.LENGTH_SHORT).show();
-                }
-                else if (tipp>gondolt){
-                    eletcsokkent();
-                    Toast.makeText(MainActivity.this, "A gondolt szám kisebb", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    builder.setTitle("Győzelem");
-                    builder.create().show();
-                }
-            }
-        });
-        konnyugomb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-        nehezgomb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-    }
-
-    private void eletcsokkent() {
-        if (elet>0){
-            elet--;
-        }
-        eletek[elet].setImageResource(R.drawable.heart1);
-        if (elet==0){
-            builder.setTitle("Vereség");
-            builder.create().show();
-        }
-    }
-
-    private void eletcsokkentSwitchCase() {
-        elet--;
+    private void eletLevon() {
+        /*
         switch (elet){
+            case 5:
+                imageHp5.setImageResource(R.drawable.heart1);
+                break;
+            case 4:
+                imageHp4.setImageResource(R.drawable.heart1);
+                break;
             case 3:
-                hp4.setImageResource(R.drawable.heart1);
+                imageHp3.setImageResource(R.drawable.heart1);
                 break;
             case 2:
-                hp3.setImageResource(R.drawable.heart1);
+                imageHp2.setImageResource(R.drawable.heart1);
                 break;
             case 1:
-                hp2.setImageResource(R.drawable.heart1);
+                imageHp1.setImageResource(R.drawable.heart1);
                 break;
-            case 0:
-                hp1.setImageResource(R.drawable.heart1);
+            default:
                 break;
         }
-        if (elet==0){
-            builder.setTitle("Vereség");
-            builder.create().show();
+         */
+        elet--;
+        eletek[elet].setImageResource(R.drawable.heart1);
+        //customToast.show();
+        if (elet < 1){
+            builderVege.setTitle("Vesztettél").create().show();
         }
     }
 
-    private void init(){
-        plus=findViewById(R.id.plus);
-        minus=findViewById(R.id.minus);
-        tippgomb=findViewById(R.id.tippgomb);
-        hp1 =findViewById(R.id.hp_1);
-        hp2 =findViewById(R.id.hp_2);
-        hp3 =findViewById(R.id.hp_3);
-        hp4 =findViewById(R.id.hp_4);
-        nehezgomb =findViewById(R.id.nehezgomb);
-        konnyugomb=findViewById(R.id.konnyugomb);
-        eletek=new ImageView[]{hp1,hp2,hp3,hp4};
-        tippertek=findViewById(R.id.tippertek);
-        rnd=new Random();
-        builder=new AlertDialog.Builder(MainActivity.this);
-        builder.setMessage("Szeretne új játékot játszani?");
-        builder.setNegativeButton("Nem", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                finish();
-            }
-        });
-        builder.setPositiveButton("Igen", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                ujJatek();
-            }
-        });
-        builder.setCancelable(false);
+    private void init() {
+        imageHp1 = findViewById(R.id.image_hp1);
+        imageHp2 = findViewById(R.id.image_hp2);
+        imageHp3 = findViewById(R.id.image_hp3);
+        imageHp4 = findViewById(R.id.image_hp4);
+        imageHp5 = findViewById(R.id.image_hp5);
+        textTippeltSzam = findViewById(R.id.text_tippelt_szam);
+        btnNovel = findViewById(R.id.btn_novel);
+        btnCsokkent = findViewById(R.id.btn_csokkent);
+        btnTippel = findViewById(R.id.btn_tipp);
+        btnKonnyu = findViewById(R.id.btn_konnyu);
+        btnNehez = findViewById(R.id.btn_nehez);
+        eletek = new ImageView[]{imageHp1,imageHp2,imageHp3,imageHp4,imageHp5};
+
+        builderVege = new AlertDialog.Builder(MainActivity.this);
+        builderVege.setCancelable(false).setMessage("Szeretne új játékot játszani?")
+                .setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ujJatek();
+                    }
+                })
+                .setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+        builderNehezseg = new AlertDialog.Builder(MainActivity.this);
+        builderNehezseg.setCancelable(false).setTitle("Nehézség váltása")
+                .setMessage("Biztosan új játékot kezd a kiválasztott nehézséggel?")
+                .setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        nehezsegAllit();
+                    }
+                })
+                .setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        customToast = Toast.makeText(MainActivity.this, "", Toast.LENGTH_LONG);
+        customToast.setGravity(Gravity.CENTER, 0,0);
+        customToast.setView(getLayoutInflater().inflate(R.layout.custom_toast,(ViewGroup)findViewById(R.id.custom_toast_layout)));
+        maxSzam = 10;
+        nehezseg = false;
+    }
+
+    private void nehezsegAllit() {
+        if (nehezseg){
+            maxSzam = 40;
+            imageHp4.setVisibility(View.VISIBLE);
+            imageHp5.setVisibility(View.VISIBLE);
+        }else{
+            maxSzam = 10;
+            imageHp4.setVisibility(View.GONE);
+            imageHp5.setVisibility(View.GONE);
+        }
         ujJatek();
     }
 
     private void ujJatek() {
-        tipp=1;
-        elet=4;
-        gondolt= rnd.nextInt(10)+1;
-        tippertek.setText(String.valueOf(tipp));
-        for (ImageView iv:eletek){
-            iv.setImageResource(R.drawable.heart2);
+        tippeltSzam = 1;
+        elet = 3;
+        if (maxSzam == 40) {
+            elet = 5;
+        }
+        gondoltSzam = (int)(Math.random()*maxSzam)+1;
+        textTippeltSzam.setText(String.valueOf(tippeltSzam));
+        /*
+        imageHp1.setImageResource(R.drawable.heart2);
+        imageHp2.setImageResource(R.drawable.heart2);
+        imageHp3.setImageResource(R.drawable.heart2);
+        imageHp4.setImageResource(R.drawable.heart2);
+        imageHp5.setImageResource(R.drawable.heart2);
+         */
+        for (ImageView elet: eletek) {
+            elet.setImageResource(R.drawable.heart2);
         }
     }
+
+
 }
